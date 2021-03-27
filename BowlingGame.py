4ee47625035facb59703127a9a973d8505c47fa8
noliosahrak <wsburghardt@gmail.com>
2021-03-27 15:42:52 +0100

    Score Spare

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,9 +1,25 @@
 class BowlingGame:
     def __init__(self):
-        self.score = 0
+        self.number_of_rolls = 0
+        self.rolls = [0 for i in range(0, 21)]
 
     def get_score(self):
-        return self.score
+        print(self.rolls)
+        score = 0
+        cursor = 0
+        for i in range(0, 10):
+            if self.is_spare(cursor):
+                score += 10
+                score += self.rolls[cursor+2]
+                cursor += 2
+            else:
+                score += self.rolls[cursor] + self.rolls[cursor+1]
+                cursor += 2
+        return score
 
     def roll(self, pins):
-        self.score += pins
+        self.rolls[self.number_of_rolls] = pins
+        self.number_of_rolls += 1
+
+    def is_spare(self, cursor):
+        return self.rolls[cursor] + self.rolls[cursor + 1] == 10
--- test_BowlingGame.py
+++ test_BowlingGame.py
@@ -12,7 +12,20 @@ class TestBowlingGame:
     def test_should_score_zero_when_no_rolls(self, game):
         assert_that(self.the_game.get_score()).is_equal_to(0)
 
-    def test_should_score_one_when_rolled_one(self, game):
-        self.the_game.roll(1)
-        assert_that(self.the_game.get_score()).is_equal_to(1)
+    def test_should_score_20_when_pin_down_20_times(self, game):
+        self.roll(20, 1)
+        assert_that(self.the_game.get_score()).is_equal_to(20)
 
+    def test_should_score_0_when_no_pin_down_20_times(self, game):
+        self.roll(20, 0)
+        assert_that(self.the_game.get_score()).is_equal_to(0)
+
+    def test_should_score_spare(self, game):
+        self.roll(2, 5)
+        self.roll(1, 4)
+        self.roll(17, 0)
+        assert_that(self.the_game.get_score()).is_equal_to(18)
+
+    def roll(self, throws, pins):
+        for i in range(0, throws):
+            self.the_game.roll(pins)
